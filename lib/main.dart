cb55d8e12a01923115a59dafa6092b31d94a8552
Kuteji <edutalavera@outlook.com>
2020-04-12 15:29:25 -0500

    convirtiendo json string enun map y creacion del future builder

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+/*Awesome Flutter Snipets
+
+  El comando mateapp => crea una aplicacion de fluuter por defecto
+*/
+
+
+
+import 'package:flutter/material.dart';
+import 'package:componentes/src/pages/home_page.dart';
+
+
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Componentes App',
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
+    );
+  }
+}
